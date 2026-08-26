55d0285b975e06760f5072c42372296c92dd3647
yalini03 <165188068+yalini03@users.noreply.github.com>
2026-08-26 22:15:04 +0800

    Update CarbonCalculator_app.py

--- CarbonCalculator_app.py
+++ CarbonCalculator_app.py
@@ -28,7 +28,7 @@ def set_background(image_file):
         unsafe_allow_html=True
     )
 
-set_background(".github/background.jpg")
+set_background("background_park.jpg")
 
 st.set_page_config(page_title = "Tree Carbon Calculator", layout = "centered")
 st.title("Biofilters: Tree Carbon Calculator")
